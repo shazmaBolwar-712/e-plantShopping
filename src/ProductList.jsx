2456e5c2df081fd7934aeba57a46ee19f867f585
shazmaBolwar-712 <shazmabolwarudupi@gmail.com>
2025-08-01 12:11:17 +0530

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useDispatch } from 'react-redux';
+import { addItem } from './CartSlice'; // adjust path if neede
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+ const [addedToCart, setAddedToCart] = useState([]);
+const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -232,7 +236,11 @@ function ProductList({ onHomeClick }) {
         fontSize: '30px',
         textDecoration: 'none',
     }
-
+ const handleAddToCart = (plantName) => {
+  if (!addedToCart.includes(plantName)) {
+    setAddedToCart([...addedToCart, plantName]);
+  }
+};
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
@@ -274,7 +282,21 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
+        {showPlants &&
+    plantsArray.map((categoryObj, index) => (
+      <div key={index}>
+        <h2 style={{ gridColumn: '1 / -1', color: '#4CAF50' }}>{categoryObj.category}</h2>
+        <div className="plant-category">
+          {categoryObj.plants.map((plant, idx) => (
+            <div key={idx} className="plant-card">
+              <h3>{plant.name}</h3>
+              <img src={plant.image} alt={plant.name} />
+              <p>{plant.description}</p>
+              <strong>{plant.cost}</strong>
+              <button className="add-to-cart-btn" 
+              onClick={() => handleAddToCart(plant.name)}
+        disabled={!!addedToCart[plant.name]}>
+        {addedToCart.includes(plant.name) ? 'Added' : 'Add to Cart'}</button>
 
                 </div>
             ) : (
